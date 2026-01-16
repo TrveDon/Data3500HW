cefc180aff98d27da95def76bbc8d98c66c3ffca
Donovan Mortensen <a02417789@usu.edu>
2026-01-16 02:58:29 +0000

    commit

--- Hw2/hw2.py
+++ Hw2/hw2.py
@@ -1,25 +1,57 @@
+#
 #2.3
-
-
-
+#
+grade = 91
+if grade >=90:
+    print('You got an A with a grade of:', grade)
+elif grade >=80:
+    print('You got a B with a grade of:', grade)
+elif grade >=70:
+    print('You got a C with a grade of:', grade)
+elif grade >=60:
+    print ('you got a D with a grade of:', grade)
+else: print('You got an F with a grade of:', grade)
+#
 #2.4
-
-
-
-
+#
+a=27.5 + 2
+print(a)
+b=27.5 - 2
+print(b)
+c=27.5 * 2
+print(c)
+d=27.5 / 2
+print(d)
+e=27.5// 2
+print(e)
+#
 #2.5
-
-
-
-
+#
+r=2
+Pi=3.15159
+diameter = r*Pi
+circumference = Pi*(r*r)
+print(diameter)
+print(circumference)
+#
 #2.6
-
-
-
-
+#
+varans=6
+rem = varans % 2
+if rem == 0:
+    print('Even')
+else: print('Odd')
+#
 #2.7
-
-
-
-
+#
+x= 1024 % 4
+if x ==0:
+    print('Yes')
+else: print('No')
+y= 2 %10
+if y ==0:
+    print('Yes')
+else: print ('No')
+#
 #2.8
+#
