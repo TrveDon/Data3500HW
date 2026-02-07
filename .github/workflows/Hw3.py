765b5ae0de6b57fc91950a2917a1b2d4f0a428a3
Donovan Mortensen <a02417789@usu.edu>
2026-02-07 03:25:28 +0000

    commit

--- .github/workflows/Hw3.py
+++ .github/workflows/Hw3.py
@@ -1,4 +1,32 @@
+#3.4
 for row in range (2):
     for col in range (7):
         print('@',end='')
     print()
+#
+#3.9
+#
+x = input('Enter a Five Digit Number: ')
+for digit in x:
+    print(digit)
+#
+#3.11
+#
+calc =[]
+for Tank in range (3):
+    print(f"Tank {Tank + 1}")
+    GalUsed = int(input("Enter Gallons Used: "))
+    Miles = int(input('Enter Miles Driven: '))
+    Milepergal = Miles / GalUsed
+    print ('The MPG for this tank was: ', Milepergal)
+    calc.append(Milepergal)
+MPGAverage = sum(calc) / len(calc)
+print("Average MPG: ", MPGAverage)
+#
+#3.12
+#
+Pal = input("Enter a Five Digit Number: ")
+if Pal == Pal[::-1]:
+    print ("This is a Palindrome")
+else: 
+    print('This is not a Palindrome')
