4b6cf28a29f1bd799d7b8e09592ef07fdb76c708
Donovan Mortensen <a02417789@usu.edu>
2026-03-19 22:48:46 +0000

    commit

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,3 +1,45 @@
 file = open("TSLA.csv")
-lines = file.readlines()
-print(lines)
+stockdata = file.readlines()
+header = stockdata[0]
+body = stockdata[1:]
+body.reverse()
+stockdata = [header] + body
+
+close_prices = [
+    float(line.strip().split(',')[1].replace('$', ''))
+    for line in stockdata[1:]
+]
+firstbuy = None
+buy = None
+profits = []
+movwin = 5
+
+for i in range (movwin, len(close_prices)):
+    current = close_prices[i]
+    prev_5 = close_prices[i -movwin:i]
+    ma_5 = sum(prev_5)/movwin
+
+    if current < ma_5 * 0.98:
+        if buy is None:
+            buy = current
+            if firstbuy is None:
+                firstbuy= current
+            print('buying at: ',current, 'Iteration', i)
+        else:
+            print('Holding at current price')
+    elif current > ma_5 *1.02:
+        if buy is not None:
+            sell = current
+            pro = sell-buy
+            profits.append(pro)
+            print('selling at: ',current)
+            buy = None
+        else:
+            print('No stock to sell')
+    else:
+        print("holding current price")
+total_profit = sum(profits)
+print('first buy price: ',firstbuy)
+print('Total profit: ',total_profit)
+percent =(total_profit/ firstbuy) * 100
+print('Percent return: ', percent)
