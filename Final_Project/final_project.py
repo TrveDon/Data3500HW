5139edae536a914450b27ca8c16d30d455d6fadf
Donovan Mortensen <a02417789@usu.edu>
2026-04-19 06:35:39 +0000

    commit

--- Final_Project/final_project.py
+++ Final_Project/final_project.py
@@ -21,42 +21,42 @@ coins = ['bitcoin','ethereum','binancecoin','tron','dogecoin','tether']
 
 def build_data():   
     for coin in coins: 
-    filename = f'{coin}_prices.csv'
+        filename = f'{coin}_prices.csv'
     
     #load existing data if present
-    existing = set()
-    if os.path.exists(filename):
-        with open(filename, 'r') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                existing.add(row['Date'])
+        existing = set()
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                   existing.add(row['Date'])
    
     #open file in append
-    file_exists = os.path.exists(filename)
-    with open(filename, 'a', newline='') as f:
-        writer = csv.writer(f)
-        #header
-        if not file_exists:
-            writer.writerow(['Date', 'Price_USD'])
+        file_exists = os.path.exists(filename)
+        with open(filename, 'a', newline='') as f:
+            writer = csv.writer(f)
+            #header
+            if not file_exists:
+                writer.writerow(['Date', 'Price_USD'])
     
-    #loop through dates to load file
-        for i in range(364):
-            dt = basedt + timedelta(days=i+1)
-            dts = dt.strftime('%d-%m-%Y')
-            if dts in existing:
-                continue
-            url = url1 + coin + url2 + dts + url3
-            req = requests.get(url)
-            d = json.loads(req.text)
-            time.sleep(12)
-            try:
-                price = d[key1][key2][key3]
-                print(dts, d[key1][key2][key3])
-                writer.writerow([dts, price])
-                f.flush()
-                existing.add(dts)
-            except Exception as e:
-                print(dts, "error: ", e)
+        #loop through dates to load file
+            for i in range(364):
+                dt = basedt + timedelta(days=i+1)
+                dts = dt.strftime('%d-%m-%Y')
+                if dts in existing:
+                    continue
+                url = url1 + coin + url2 + dts + url3
+                req = requests.get(url)
+                d = json.loads(req.text)
+                time.sleep(12)
+                try:
+                    price = d[key1][key2][key3]
+                    print(f"[{coin}] {dts} {price}")
+                    writer.writerow([dts, price])
+                    f.flush()
+                    existing.add(dts)
+                except Exception as e:
+                    print(dts, "error: ", e)
 
 #------------File Loader---------
 def load_prices(filename):
